7c1aa6290126d1aabc85ee0ae23e774a5839cd89
ADB-DevWorks <adam.brawley@snhu.edu>
2026-04-29 20:11:44 -0500

    Setup login of health app

---
+++ WidgetHub.Core/Health.Models/HealthUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WidgetHub.Core.Health.Models
+{
+    //Class is public to import out of .Core
+    public class HealthUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
